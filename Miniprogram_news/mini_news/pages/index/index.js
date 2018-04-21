241aeca1c9cc6e870c6e6651a70caec66c48e60b
Sophie521 <18782249841@163.com>
2018-04-21 15:31:53 +0800

    get news json list and detail 3-2

--- Miniprogram_news/mini_news/pages/index/index.js
+++ Miniprogram_news/mini_news/pages/index/index.js
@@ -1,5 +1,24 @@
+
 Page({
-  onLoad(){
-    console.log("hello world~!")
+  onLoad() {
+    console.log("hello world~!"),
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: 'gn'
+      },
+      success: res => {
+        console.log(res)
+      }
+    }),
+      wx.request({
+        url: 'https://test-miniprogram.com/api/news/detail',
+        data: {
+          id: '1523074607642'
+        },
+        success: res => {
+          console.log(res)
+        }
+      })
   }
 })
