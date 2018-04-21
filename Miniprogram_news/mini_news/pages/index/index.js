fe475d255ff20cfc2be9c82eeb56f3400c606df5
Sophie521 <18782249841@163.com>
2018-04-21 16:06:45 +0800

    get news result 3-3

--- Miniprogram_news/mini_news/pages/index/index.js
+++ Miniprogram_news/mini_news/pages/index/index.js
@@ -1,7 +1,8 @@
-
 Page({
   onLoad() {
     console.log("hello world~!"),
+    
+    //列表页
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
       data: {
@@ -9,8 +10,16 @@ Page({
       },
       success: res => {
         console.log(res)
+        let result = res.data.result
+        let news_list_date = result[0].date
+        let news_list_image = result[0].firstImage
+        let news_list_id = result[0].id
+        let news_list_title = result[0].title
+        console.log(news_list_date, news_list_id, news_list_image,news_list_title)
       }
     }),
+
+    //详情页
       wx.request({
         url: 'https://test-miniprogram.com/api/news/detail',
         data: {
@@ -18,6 +27,11 @@ Page({
         },
         success: res => {
           console.log(res)
+          let result = res.data.result.content
+          let news_detail_id = result[0].id
+          let news_detail_text = result[0].text
+          let news_detail_type = result[0].type
+          console.log(news_detail_id, news_detail_text, news_detail_type)
         }
       })
   }
